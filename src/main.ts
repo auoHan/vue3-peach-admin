435f4622f2efdd182eab307cfeb459cc5e65d8dd
auoHan <710543268@qq.com>
2022-04-24 03:56:43 +0800

    全局使用路由

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import App from './App.vue'
 import piniaPluginPersist from 'pinia-plugin-persist'
+import router from '@/router'
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersist)
 app.use(pinia)
+app.use(router)
 app.mount('#app')
